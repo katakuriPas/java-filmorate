d891ea424c6d8a3cec5373911225691138dfdf87
Pasha Pas <pavlik_pas@mail.ru>
2026-02-10 22:35:18 +0300

    Filmorate#2

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -22,7 +21,7 @@ public class Film {
     private int duration;
     private Set<Long> idUsersLike;
 
-    public int getSumLikes(){
+    public int getSumLikes() {
         return idUsersLike.size();
     }
 }
