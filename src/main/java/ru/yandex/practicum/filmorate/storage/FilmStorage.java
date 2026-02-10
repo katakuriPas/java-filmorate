5c664ead1dcc6018f134f82affe6d1df534d9fac
Pasha Pas <pavlik_pas@mail.ru>
2026-02-10 22:48:47 +0300

    shitCheckStyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 
 public interface FilmStorage {
+
     Collection<Film> findAllFilm();
     Film createFilm(Film film);
     Film updateFilm(Film newFilm);
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
 
 public interface UserStorage {
+
     Collection<User> findAllUser();
     User createUser(User user);
     User updateUser(User newUser);
